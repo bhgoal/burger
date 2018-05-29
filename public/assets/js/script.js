3a8684c7c1e3ad0695bfcc0d6820d9961c53612d
gorbasho <37026147+gorbasho@users.noreply.github.com>
2018-05-28 21:23:13 -0500

    ui 1.0

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,18 @@
+$(window).on("load", function(){
+    $('img.bgfade').hide();
+    var dg_H = $(window).height();
+    var dg_W = $(window).width();
+    $('#wrap').css({'height':dg_H,'width':dg_W});
+    function anim() {
+        $("#wrap img.bgfade").first().appendTo('#wrap').fadeOut(2000);
+        $("#wrap img").first().fadeIn(2000);
+        setTimeout(anim, 5000);
+    }
+    anim();
+})
+$(window).resize(function(){window.location.href=window.location.href})
+
+
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
     $(".eat-burger").on("click", function(event) {
